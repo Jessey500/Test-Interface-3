dcee1313f29ec0cbe5048bd480910a4bc8cf5d47
Jesey500 <Jess.jesey@gmail.com>
2020-11-30 20:05:24 +0600

    Test Interface 3

---
+++ Test Interface 3/RadioPlayerInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Interface_3
+{
+    interface RadioPlayerInterface
+    { 
+        void Switch(Boolean on);
+        void retune(double frequency);
+        void setVolume(int loudness);
+        void ChangeChannel();
+    
+
+    }
+}
